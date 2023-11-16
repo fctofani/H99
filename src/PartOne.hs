a6c306d30a40d59512ef017e42daebd572dd0fb9
Fabricio César Tofani <fctofani2@gmail.com>
2023-11-16 17:55:15 -0300

    solving Q2 to Q4

--- src/PartOne.hs
+++ src/PartOne.hs
@@ -1,6 +1,28 @@
-module PartOne (myLast) where
+module PartOne (myLast, mySecLast, findElem, myLen) where
 
+-- Q1) Get the last element of a list
 myLast :: [a] -> a
 myLast [] = error "no last elem on empty list"
 myLast [x] = x
 myLast (_ : xs) = myLast xs
+
+-- Q2) Find second last element of a list
+mySecLast :: [a] -> a
+mySecLast [] = error "no second last elem on empty list"
+mySecLast [_] = error "no second last on one-elem list"
+mySecLast [x, _] = x
+mySecLast (_ : xs) = mySecLast xs
+
+-- Q3) find the k element on a list (starting at index 1)
+findElem :: [a] -> Integer -> a
+findElem [] _ = error "not found"
+findElem (x : _) 1 = x
+findElem (_ : xs) n = findElem xs (n - 1)
+
+-- Q4) find the length of a list
+myLen' :: [a] -> Integer -> Integer
+myLen' [] n = n
+myLen' (_ : xs) n = myLen' xs (n + 1)
+
+myLen :: [a] -> Integer
+myLen xs = myLen' xs 0
--- test/PartOneSpec.hs
+++ test/PartOneSpec.hs
@@ -6,7 +6,19 @@ import Test.Hspec
 spec :: Spec
 spec = do
   describe "Part One" $ do
-    it "exercise 1 test 1" $ do
+    it "exercise 01 test A" $ do
       shouldBe (myLast [1, 2, 3, 4]) 4
-    it "exercise 1 test 2" $ do
+    it "exercise 01 test B" $ do
       shouldBe (myLast ['x', 'y', 'z']) 'z'
+    it "exercise 02 test A" $ do
+      shouldBe (mySecLast [1,2,3,4]) 3
+    it "exercise 02 test B" $ do
+      shouldBe (mySecLast ['a'..'z']) 'y'
+    it "exercise 03 test A" $ do
+      shouldBe (findElem [1, 2, 3] 2) 2
+    it "exercise 03 test B" $ do
+      shouldBe (findElem "haskell" 5) 'e'
+    it "exercise 04 test A" $ do
+      shouldBe (myLen [123, 456, 789]) 3
+    it "exercise 04 test B" $ do
+      shouldBe (myLen "Hello, world!") 13
